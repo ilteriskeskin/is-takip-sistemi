59ad18e6f3399dc3ad0aa37fdd3af60b83f01caa
Ali İlteriş Keskin <aliilteriskeskin@gmail.com>
2018-06-08 03:58:09 +0300

    Projenin sonlarına doğru

--- blog/article/urls.py
+++ blog/article/urls.py
@@ -2,9 +2,13 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
-app_name = "article"
+app_name = "job"
 
 urlpatterns = [
     path('dashboard/', views.dashboard, name = "dashboard"),
-    path('addarticle/', views.addArticle, name = "addarticle"),
+    path('addarticle/', views.addarticle, name = "addarticle"),
+    path('article/<int:id>', views.detail, name = "detail"),
+    path('update/<int:id>', views.updateArticle, name = "update"),
+    path('delete/<int:id>', views.deleteArticle, name = "delete"),
+    path('', views.articles, name = "articles"),
 ]
--- blog/article/views.py
+++ blog/article/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect, get_object_or_404
+from .forms import ArticleForm
+from .models import Article
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -8,8 +12,59 @@ def index(request):
 def about(request):
     return render(request, "about.html")
 
-def dashboard(request):
-    return render(request, "dashboard.html")
+def articles(request):
+    articles = Article.objects.all()
 
-def addArticle(request):
-    return render(request, "addarticle.html")
+    return render(request, "articles.html", {"articles":articles})
+
+@login_required(login_url='/user/login/')
+def dashboard(request):
+    articles = Article.objects.filter(author = request.user)
+    context = {
+        "articles":articles
+    }
+
+    return render(request, "dashboard.html", context)
+
+@login_required(login_url='/user/login/')
+def addarticle(request):
+    form = ArticleForm(request.POST or None)
+
+    if form.is_valid():
+        article = form.save(commit = False)
+        article.author = request.user
+        article.save()
+
+        messages.success(request, "Görev Kaydı Başarılı Bir Şekilde Alındı.")
+
+        return redirect("index")
+
+    return render(request, "addarticle.html", {"form":form})
+
+def detail(request, id):
+    article = get_object_or_404(Article, id = id)
+    return render(request, "detail.html", {"article":article})
+
+@login_required(login_url='/user/login/')
+def updateArticle(request, id):
+    article = get_object_or_404(Article, id = id)
+    form = ArticleForm(request.POST or None, instance = article)
+    if form.is_valid():
+        article = form.save(commit = False)
+        article.author = request.user
+        article.save()
+
+        messages.success(request, "Görev Başarılı Bir Şekilde Güncellendi.")
+
+        return redirect("index")
+
+    return render(request, "update.html", {"form":form})
+
+@login_required(login_url='/user/login/')
+def deleteArticle(request, id):
+    article = get_object_or_404(Article, id = id)
+    article.delete()
+
+    messages.success(request, "Görev Başarılı Bir Şekilde Silindi.")
+
+    return redirect("index")
