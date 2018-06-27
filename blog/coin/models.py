e62eb91074d19f2b6f754069bdb21da1b052477c
Ali İlteriş Keskin <aliilteriskeskin@gmail.com>
2018-06-27 23:00:56 +0300

    Admin Paneli Düzenlendi

--- blog/coin/models.py
+++ blog/coin/models.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 # Create your models here.
 
 class Coin(models.Model):
-    author = models.CharField(max_length = 20, verbose_name = "Coin Sahibi")
+    author = models.ForeignKey("auth.User", on_delete = models.CASCADE, verbose_name = "Yazar")
     content = models.TextField(verbose_name = "Neden")
     quantity = models.CharField(max_length = 10, verbose_name = "Miktar")
     created_date = models.DateTimeField(auto_now_add = True, verbose_name = "Oluşturulma Tarihi")
