664e8b5ca7202a558cdcd5c51c4a913cbba2b741
Ali İlteriş Keskin <ilteriskesk20@gmail.com>
2018-06-30 15:14:35 +0300

    Kayıt ol kısmı silindi

--- blog/user/urls.py
+++ blog/user/urls.py
@@ -5,7 +5,6 @@ from . import views
 app_name = "user"
 
 urlpatterns = [
-    path('register/', views.register, name = "register"),
     path('login/', views.loginUser, name = "login"),
     path('logout/', views.logoutUser, name = "logout"),
 ]
--- blog/user/views.py
+++ blog/user/views.py
@@ -8,28 +8,6 @@ from article.models import Article
 
 # Create your views here.
 
-def register(request):
-    form = RegisterForm(request.POST or None)
-
-    if form.is_valid():
-        username = form.cleaned_data.get("username")
-        password = form.cleaned_data.get("password")
-
-        newUser = User(username = username)
-        newUser.set_password(password)
-        newUser.save()
-        login(request, newUser)
-
-        messages.success(request, "Kayıt başarılı, giriş sayfasına yönlendirildiniz.")
-        
-        return redirect("index")
-
-    context = {
-        "form" : form
-    }
-
-    return render(request, "register.html", context)
-
 def loginUser(request):
     form = LoginForm(request.POST or None)
 
